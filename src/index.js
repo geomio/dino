8a8a12ac9a20fcb203275b9f8ad3da9e3c7a420c
Geoffry Millner <geoff.millner22@gmail.com>
2021-10-29 15:16:30 -0700

    made base dino project files button and added api

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+$(document).ready(function () {
+
+  let request = new XMLHttpRequest();
+  const url = `https://dinoipsum.com/api/?format=json`;
+
+});
+
